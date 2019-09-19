e8946eb06e449582ab4f755e0a389e357db68378
Lars Bekhof <larsbekhof@gmail.com>
2019-09-19 22:06:42 +0200

    no message

--- volume.py
+++ volume.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 import sys
 
-NO_SINK_CHAR = '-%'
+lastKnownVolumeFile = '/home/lars/Code/i3-volume-script/.last-known-volume'
 
 result = subprocess.run(['pactl', 'list', 'sinks'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
@@ -19,14 +19,19 @@ for x in range(len(splitOutput)):
 # These regexs can probably be more elegant
 output = re.search('Volume:.*/.*([0-9]{2}|[0-9]%).*/', runningOutput)
 
+lastKnownVolume  = open(lastKnownVolumeFile).read()
+
 if output is None or len(output.group(0).strip()) == 0:
-    print(NO_SINK_CHAR)
+    print(lastKnownVolume)
     sys.exit()
 
 output = re.search('[0-9]*%', output.group(0))
 
 if output is None or len(output.group(0).strip()) == 0:
-    print(NO_SINK_CHAR)
+    print(lastKnownVolume)
     sys.exit()
+
+f = open(lastKnownVolumeFile, 'w+')
+f.write(output.group(0))
 
 print(output.group(0))
